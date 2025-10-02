98972f58aa9295651ff8949212acda3e3bb0d883
Ngoc Ha <ha69630@gmail.com>
2025-10-02 20:31:12 +0700

    Clean trash

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,9 +1,9 @@
-const newsRouter = require('./news');
+// const newsRouter = require('./news');
 const siteRouter = require('./site');
 const coursesRouter = require('./courses');
 
 function route(app) {
-    app.use('/news', newsRouter);
+    // app.use('/news', newsRouter);
     app.use('/', siteRouter);
     app.use('/courses', coursesRouter);
 }
