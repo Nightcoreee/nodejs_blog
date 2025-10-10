fb47f818605c5846caa75c39b8074da866d4402f
Ngoc Ha <ha69630@gmail.com>
2025-10-10 21:34:42 +0700

    add event checkbox

--- src/app/controllers/CourseController.js
+++ src/app/controllers/CourseController.js
@@ -71,6 +71,19 @@ class CourseController {
             )
             .then(() => res.redirect('/me/stored/courses'));
     }
+
+    // [POST] /courses/handle-form-actions
+    handleFormActions(req, res, next) {
+        switch (req.body.action) {
+            case 'delete':
+                Course.delete({ _id: { $in: req.body.courseIds } })
+                    .then(() => res.redirect('back'))
+                    .catch(next);
+                break;
+            default:
+                res.json({ message: 'Hành động không hợp lệ!' });
+        }
+    }
 }
 
 module.exports = new CourseController();
